3af43305c7b09f01e8b374301fc75823cc0707c3
douliou50218 <34338937+douliou50218@users.noreply.github.com>
2020-06-20 04:43:47 +0800

    修改支付方法

--- dist/payForm/script.js
+++ dist/payForm/script.js
@@ -2,5 +2,28 @@
 
 $(document).ready(function(){
     $("img:eq(1)").css("height",($("img.w-100").css("height")));
+    cardFormShow();
   })
-  
+
+
+$('input[name=amount]').change(function(){
+  var amount = $('input[name=amount]:checked').val();
+  console.log(amount);
+});
+
+$('input[name=payment]').change(function(){
+  if ($('input[name=payment]:checked').attr("id")=='tools-1') {
+    cardFormShow();
+  } else {
+    myCardFormShow();
+  }
+});
+
+function cardFormShow() {
+  $('form[name=card]').show();
+  $('form[name=myCard]').hide();
+}
+function myCardFormShow() {
+  $('form[name=card]').hide();
+  $('form[name=myCard]').show();
+}
